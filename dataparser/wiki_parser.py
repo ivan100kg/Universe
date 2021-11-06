334dad839ad757e14eeac05b9dbc28899411e365
ivar <ivar110kg@gmail.com>
2021-11-06 19:15:22 +0400

    add app 'constellation'

--- dataparser/wiki_parser.py
+++ dataparser/wiki_parser.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 
 
 def main():
+    """Getting constellations' names and links"""
     url = 'https://en.wikipedia.org/wiki/IAU_designated_constellations'
     headers = {
         'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36'}
@@ -10,15 +11,13 @@ def main():
     soup = BeautifulSoup(resp.text, 'html.parser')
     table = soup.find('table', {'class': 'wikitable'})
     rows = table.find_all('tr')
-    constellations = {}
+    constellations = []
     for row in rows:
         tag_a = row.find('a')
         if tag_a and 'wiki' in tag_a['href']:
-            constellations[tag_a.text] = 'https://en.wikipedia.org/'+tag_a['href']
-    print('Len', len(constellations))
-    for c in constellations:
-        print(c, '==>', constellations[c])
+            constellations.append((tag_a.text, 'https://en.wikipedia.org/'+tag_a['href']))
+    return constellations
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
