d03097e0d17da84a3a3176ca92c3117d9f5bc53b
ivar <ivar110kg@gmail.com>
2021-11-28 12:00:28 +0400

    update views with classes + mixins

--- constellations/views.py
+++ constellations/views.py
@@ -11,6 +11,9 @@ from rest_framework.response import Response
 from django.http import Http404
 from rest_framework.views import APIView
 
+from rest_framework import mixins
+from rest_framework import generics
+
 
 # ------------------- without DRF ---------------------------------
 # @csrf_exempt
@@ -102,54 +105,94 @@ from rest_framework.views import APIView
 #     elif request.method == 'DELETE':
 #         snippet.delete()
 #         return Response(status=status.HTTP_204_NO_CONTENT)
+# ===============================================================================================
+
 
 # ------------------- with classes(APIView) ---------------------------------
-class ConstellationsList(APIView):
-    """
-    List all snippets, or create a new snippet.
-    """
+# class ConstellationsList(APIView):
+#     """
+#     List all snippets, or create a new snippet.
+#     """
+#
+#     def get(self, request, format=None):
+#         snippets = Constellation.objects.all()
+#         serializer = ConstellationSerializer(snippets, many=True)
+#         return Response(serializer.data)
+#
+#     def post(self, request, format=None):
+#         serializer = ConstellationSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#
+# class ConstellationDetail(APIView):
+#     """
+#     Retrieve, update or delete a snippet instance.
+#     """
+#
+#     def get_object(self, pk):
+#         try:
+#             return Constellation.objects.get(pk=pk)
+#         except Constellation.DoesNotExist:
+#             raise Http404
+#
+#     def get(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = ConstellationSerializer(snippet)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         serializer = ConstellationSerializer(snippet, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#
+#     def delete(self, request, pk, format=None):
+#         snippet = self.get_object(pk)
+#         snippet.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+# ===============================================================================================
 
-    def get(self, request, format=None):
-        snippets = Constellation.objects.all()
-        serializer = ConstellationSerializer(snippets, many=True)
-        return Response(serializer.data)
+# ------------------- with classes(APIView) ---------------------------------
+class ConstellationsList(mixins.ListModelMixin,
+                         mixins.CreateModelMixin,
+                         generics.GenericAPIView):
+    queryset = Constellation.objects.all()
+    serializer_class = ConstellationSerializer
 
-    def post(self, request, format=None):
-        serializer = ConstellationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class ConstellationDetail(APIView):
-    """
-    Retrieve, update or delete a snippet instance.
-    """
+class ConstellationDetail(mixins.RetrieveModelMixin,
+                          mixins.UpdateModelMixin,
+                          mixins.DestroyModelMixin,
+                          generics.GenericAPIView):
+    queryset = Constellation.objects.all()
+    serializer_class = ConstellationSerializer
 
-    def get_object(self, pk):
-        try:
-            return Constellation.objects.get(pk=pk)
-        except Constellation.DoesNotExist:
-            raise Http404
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = ConstellationSerializer(snippet)
-        return Response(serializer.data)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = ConstellationSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+# ===============================================================================================
 
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+
+
 
 # from rest_framework import permissions
 # from rest_framework.viewsets import ModelViewSet
